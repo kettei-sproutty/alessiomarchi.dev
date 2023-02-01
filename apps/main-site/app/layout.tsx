7072b5b9629f37d1fa236525635f48260c9f35a0
alessio.marchi <alessio.marchi@accenture.com>
2023-02-01 22:43:12 +0100

    feat: add theming

--- apps/main-site/app/layout.tsx
+++ apps/main-site/app/layout.tsx
@@ -1,15 +1,25 @@
 import React, { type FC, type PropsWithChildren } from "react";
+import { cookies } from 'next/headers'
 import { AnalyticsWrapper } from '@alessiomarchi/ui/components'
-import "@alessiomarchi/ui/styles/globals.css";
 import Navbar from "../components/navbar/navbar";
+import clsx from "clsx";
+
+import "@alessiomarchi/ui/styles/globals.css";
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
+  const themeCookie = cookies().get('theme')
+  
+  const classes = clsx({
+    '[color-scheme:dark]': themeCookie?.value === 'dark',
+    '[color-scheme:light]': themeCookie?.value === 'light'
+  })
+
   return (
-    <html lang="en" className="[color-scheme:dark]">
+    <html lang="en" className={classes}>
       <head />
       <body className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
         <Navbar />
-        <main className="px-2">{children}</main>
+        <main className="px-2 overflow-y-scroll max-h-screen">{children}</main>
         <AnalyticsWrapper />
       </body>
     </html>
--- apps/main-site/components/navbar/navbar.tsx
+++ apps/main-site/components/navbar/navbar.tsx
@@ -1,15 +1,18 @@
 import React, { type FC } from "react";
 import { Link } from "@alessiomarchi/ui/components";
+import ChangeTheme from "../theme/change-theme";
 
 import styles from './navbar.module.css';
 
 const Navbar: FC = () => {
+
   return (
     <nav className={styles.root}>
       <Link href="/">Home</Link>
       <Link href="/about">About me</Link>
       <Link href="/projects">Projects</Link>
       <Link href="/design">Design</Link>
+      <ChangeTheme />
     </nav>
   );
 };

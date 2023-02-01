1e2a2b94fa6d39ef00408a9669682572eac76537
alessio.marchi <alessio.marchi@accenture.com>
2023-02-01 11:01:45 +0100

    fix: manifest and icons

--- apps/main-site/app/head.tsx
+++ apps/main-site/app/head.tsx
@@ -6,9 +6,9 @@ const Head: FC = () => {
       <title>Alessio Marchi | Home</title>
       <meta name="description" content="Alessio Marchi personal website" />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
       <link rel="icon" href="/favicon.ico" />
     </Fragment>
   );
--- apps/main-site/next.config.js
+++ apps/main-site/next.config.js
@@ -1,7 +1,6 @@
 const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   dest: "public",
-  sw: "service-worker.gen.js",
 });
 
 /** @type {import('next').NextConfig} */

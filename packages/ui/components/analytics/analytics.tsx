b8d1b945e95b0859dfcda6bf09a89bf17b901cb2
alessio.marchi <alessio.marchi@accenture.com>
2023-02-01 17:04:31 +0100

    fix: analytics wrapper

---
+++ packages/ui/components/analytics/analytics.tsx
@@ -0,0 +1,9 @@
+'use client';
+import React, { type FC } from 'react'
+import { Analytics } from '@vercel/analytics/react';
+
+const AnalyticsWrapper: FC = () => {
+  return <Analytics />;
+}
+
+export default AnalyticsWrapper
